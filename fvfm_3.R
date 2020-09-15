7f127b12df599159f8a93a4c8cf330f22271e5a9
h-nagoe <nagoe.hkr@gmail.com>
2020-09-15 11:18:35 +0900

    読み込みデータ修正

--- fvfm_3.R
+++ fvfm_3.R
@@ -46,6 +46,9 @@ dset = read_xlsx(dir(pattern = "200821_wakame_168h_2*.*xlsx",
 #1個だけ項目名を変換したい
 names(dset)[ which( names(dset)=="Fv/Fm" ) ] = "FvFm"
 
+dset = dset %>% 
+  select(Hour,Growth,temperature = Temp,fvfm =FvFm)
+
 # data24 = dset %>% 
 #   group_by(Growth) %>% 
 #   filter(Hour == 24) %>% 
@@ -54,37 +57,27 @@ names(dset)[ which( names(dset)=="Fv/Fm" ) ] = "FvFm"
 # H,Gごとにデータ読み込む ----
 
 data24M = dset %>% 
-  filter(Hour == 24,Growth == "Mature") %>% 
-  select(Hour,Growth,temperature = Temp,fvfm =FvFm)
+  filter(Hour == 24,Growth == "Mature")
 
 data24J = dset %>% 
-  filter(Hour == 24,Growth == "Juvenile") %>% 
-  select(Hour,Growth,temperature = Temp,fvfm =FvFm)
+  filter(Hour == 24,Growth == "Juvenile")
 
 data48M = dset %>% 
-  filter(Hour == 48,Growth == "Mature") %>% 
-  select(Hour,temperature = Temp,fvfm =FvFm)
+  filter(Hour == 48,Growth == "Mature") 
 
 data48J = dset %>% 
-  filter(Hour == 48,Growth == "Juvenile") %>% 
-  select(Hour,temperature = Temp,fvfm =FvFm)
+  filter(Hour == 48,Growth == "Juvenile")
 
 data72M = dset %>% 
-  filter(Hour == 72,Growth == "Mature") %>% 
-  select(Hour,temperature = Temp,fvfm =FvFm)
+  filter(Hour == 72,Growth == "Mature")
 
 data72J = dset %>% 
-  filter(Hour == 72,Growth == "Juvenile") %>% 
-  select(Hour,temperature = Temp,fvfm =FvFm)
+  filter(Hour == 72,Growth == "Juvenile") 
 
 data168M = dset %>% 
-  filter(Hour == 168,Growth == "Mature") %>% 
-  select(Hour,temperature = Temp,fvfm =FvFm)
-
+  filter(Hour == 168,Growth == "Mature")
 data168J = dset %>% 
-  filter(Hour == 168,Growth == "Juvenile") %>% 
-  select(Hour,temperature = Temp,fvfm =FvFm)
-
+  filter(Hour == 168,Growth == "Juvenile")
 
 dset %>% 
   ggplot() + 
@@ -135,9 +128,9 @@ PRIORS = set_prior(prs %>% filter(parameter == "eta") %>% pull(prior),
   set_prior(prs %>% filter(parameter == "ha") %>% pull(prior),
             class = "b", lb = 0, nlpar = "HA")
 
-
-brmout24M = brm(brmsmodel, 
-                data = data24M %>% filter(temperature < 30),
+# 仮当てはめ
+brmout = brm(brmsmodel, 
+                data = dset %>% filter(temperature < 30),
                 stanvars = stanvars, prior = PRIORS,
                 iter = 10, chains = 4, cores = 4, seed = 2020,
                 control = list(adapt_delta = 0.9999, max_treedepth = 12))
@@ -145,11 +138,11 @@ brmout24M = brm(brmsmodel,
 # Mature
 dset2 = dset %>% 
   filter(Growth != "Juvenile") %>% 
-  filter(Temp < 30) %>% 
-  select(Hour,temperature = Temp,fvfm =FvFm) %>% 
+  filter(temperature < 30) %>% 
+  select(Hour,temperature,fvfm) %>% 
   group_nest(Hour) %>% 
   mutate(bfit = map(data, function(df) {
-    update(brmout24M, 
+    update(brmout, 
            newdata = df, 
            stanvars = stanvars, prior = PRIORS,
            iter = 5000, chains = 4, cores = 4, seed = 2020,
@@ -159,11 +152,11 @@ dset2 = dset %>%
 # Juvenile
 dset2J = dset %>% 
   filter(Growth == "Juvenile") %>% 
-  filter(Temp < 30) %>% 
-  select(Hour,temperature = Temp,fvfm =FvFm) %>% 
+  filter(temperature < 30) %>% 
+  select(Hour,temperature,fvfm) %>% 
   group_nest(Hour) %>% 
   mutate(bfit = map(data, function(df) {
-    update(brmout24M, 
+    update(brmout, 
            newdata = df, 
            stanvars = stanvars, prior = PRIORS,
            iter = 5000, chains = 4, cores = 4, seed = 2020,
@@ -172,7 +165,7 @@ dset2J = dset %>%
 
 expose_functions(brmout24M, vectorize = TRUE)
 
-dset2 %>% 
+dset2 = dset2 %>% 
   mutate(summary = map(bfit, summary)) %>% 
   mutate(ppc_dens_overlay = map(bfit, function(bfit) {
     y = bfit$data %>% pull(fvfm)
@@ -204,24 +197,71 @@ dset2 = dset2 %>%
       mean_hdci()
   }))
 
+
+dset2J = dset2J %>% 
+  mutate(summary = map(bfit, summary)) %>% 
+  mutate(ppc_dens_overlay = map(bfit, function(bfit) {
+    y = bfit$data %>% pull(fvfm)
+    yrep = posterior_predict(bfit, nsamples = 500)
+    ppc_dens_overlay(y, yrep)
+  })) %>% 
+  mutate(mcmc_rank_overlay = map(bfit, function(bfit){
+    posterior_out = as.array(bfit)
+    mcmc_rank_overlay(posterior_out)      
+  }))
+
+dset2J = dset2J %>% 
+  mutate(predict = map(bfit, function(bfit) {
+    bfit$data %>% 
+      expand(temperature = seq(min(temperature), 
+                               max(temperature),
+                               by = 1)) %>% 
+      add_predicted_draws(bfit) %>% 
+      group_by(temperature) %>% 
+      mean_hdci()
+  })) %>% 
+  mutate(expect = map(bfit, function(bfit) {
+    bfit$data %>% 
+      expand(temperature = seq(min(temperature), 
+                               max(temperature),
+                               by = 1)) %>% 
+      add_linpred_draws(bfit) %>% 
+      group_by(temperature) %>% 
+      mean_hdci()
+  }))
+
 xlabF = expression("Temperature" ~ ({}^degree*C ))
 ylabF = "Maximum Quantum Yield (Fv/Fm)" 
-ggplot() + 
-  geom_ribbon(aes(ymin = .lower, ymax = .upper, x = temperature,
-                  fill = Hour),
+
+
+plot = ggplot() + 
+  geom_ribbon(aes(ymin = .lower, ymax = .upper,x = temperature),
               alpha = 0.2,
-              data = dset2 %>% unnest(predict)) +
-  geom_line(aes(x = temperature, y = .value, color = Hour),
-            data = dset %>% unnest(expect)) +
-  geom_point(aes(x = temperature, y = fvfm, color = Hour),
+              data = dset2 %>% unnest(predict),
+              fill = "seagreen") +
+  geom_line(aes(x = temperature, y = .value,colour = "seagreen"),
+            data = dset2 %>% unnest(expect)) +
+  geom_point(aes(x = temperature, y = fvfm, colour = "seagreen"),
              data = dset2 %>% unnest(data)) +
+  geom_ribbon(aes(ymin = .lower, ymax = .upper,x = temperature),
+              alpha = 0.2,
+              data = dset2J %>% unnest(predict),
+              fill = "salmon") +
+  geom_line(aes(x = temperature, y = .value,colour = "salmon"),
+            data = dset2J %>% unnest(expect)) +
+  geom_point(aes(x = temperature, y = fvfm,colour = "salmon"),
+             data = dset2J %>% unnest(data)) +
+  facet_wrap("Hour")+
+  theme_pubr()
+
+plot
+
   ggtitle("a")+
   annotate("text",x = 35, y = 0.95,label = "24-h")+
   scale_x_continuous(xlabF,limits = c(4, 36),breaks = seq(4, 36, by = 4)) +
   scale_y_continuous(ylabF,limits = c(0, 1.0),breaks = seq(0,1.0, by = 0.2)) +
-  
-  theme_pubr()+
   theme(legend.position = c(0.1,0.2),
         legend.background = element_blank(),
         plot.title = element_text(vjust = -6,hjust = 0.02))
 
+ggsave("plotM.jpg",plot = plotM)
